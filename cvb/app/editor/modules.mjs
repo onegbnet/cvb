a8f3c7ec84fe0292885002b4d3d43173ac6b17bd
Daniel Taka <pass@one.gb.net>
2026-08-18 08:51:08 +0800

    release: sort complete country code options

--- cvb/app/editor/modules.mjs
+++ cvb/app/editor/modules.mjs
@@ -5,6 +5,9 @@
 // 校验(validate):email | phone | url(打字时经 ccs Field,提交时兜底)
 import { tr } from '../lib/i18n.mjs';
 
+const ISO_COUNTRY_CODES = `AD AE AF AG AI AL AM AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW`.split(' ');
+const COUNTRY_CODE_OPTIONS = [...ISO_COUNTRY_CODES].sort().map((value) => ({ value, label: value }));
+
 const input = (attributeId, labelKey, opts = {}) => ({ type: 'input', attributeId, labelKey, ...opts });
 const month = (attributeId, labelKey, opts = {}) => ({ type: 'month', attributeId, labelKey, ...opts });
 
@@ -94,7 +97,7 @@ export const MODULES = [
     get: (r) => ({ ...(r.basics.location || {}) }),
     set: (r, v) => ({ ...r, basics: { ...r.basics, location: { ...r.basics.location, ...v } } }),
     fields: [
-      { type: 'select', attributeId: 'countryCode', labelKey: 'field.basics.countryCode', options: ['CN', 'AU', 'JP', 'US', 'GB', 'CA', 'DE', 'FR', 'IN', 'SG'].map((value) => ({ value, label: value })) },
+      { type: 'select', attributeId: 'countryCode', labelKey: 'field.basics.countryCode', options: COUNTRY_CODE_OPTIONS },
       input('region', 'field.basics.region', { placeholderKey: 'field.basics.region.hint' }),
       input('city', 'field.basics.city'), input('address', 'field.basics.address'), input('postalCode', 'field.basics.postalCode'),
     ],
